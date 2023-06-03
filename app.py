ec8b08011ae27c2230e0adc64c3c5d9a9324f285
mishagunya <135122777+mishagunya@users.noreply.github.com>
2023-06-03 11:28:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,43 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!,  новая жопа наступила!!'
+
+
+
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_cross(image_path, color, is_horizontal):
+    img = Image.open(image_path).convert('RGB')
+    pixels = img.load()
+    width, height = img.size
+
+    if is_horizontal:
+        for i in range(width):
+            pixels[i, height // 2] = color
+    else:
+        for j in range(height):
+            pixels[width // 2, j] = color
+
+    img.save('output.png')
+
+   
+    # Plot color distribution of output image
+    img_output = Image.open('output.png')
+    r, g, b = img_output.split()
+    colors = ('r', 'g', 'b')
+    for channel, color in zip((r, g, b), colors):
+        plt.hist(np.array(channel).ravel(), bins=256, color=color, alpha=0.5)
+
+    img1 = Image.open(image_path)
+# Show original and output images
+    fig, axs = plt.subplots(1)
+    
+    axs.imshow(img_output)
+    axs.set_title('Output Image')
+
+    plt.show()
+
+# Example usage:
+draw_cross('/mntDrive/MyDrive/python/dataf/1.jpg', (255, 0, 0), is_horizontal=False)
