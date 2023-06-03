4fa4b59e528a7ae026b893f103bd7ce5b89978c3
mishagunya <135122777+mishagunya@users.noreply.github.com>
2023-06-03 20:08:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!,  новая жопа наступила!!'
+    return 'Пробная строка для проверки деплоя на render.com!'
 
 
 
